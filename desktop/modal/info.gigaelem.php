3cb9095a6e3a41c11d75cfa35b3158128d7de963
astro-sam <77665940+astro-sam@users.noreply.github.com>
2021-01-19 10:57:52 +0100

    Add files via upload

---
+++ desktop/modal/info.gigaelem.php
@@ -0,0 +1,47 @@
+<?php
+/* This file is part of Plugin openzwave for jeedom.
+ *
+ * Plugin openzwave for jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Plugin openzwave for jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Plugin openzwave for jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+if (!isConnect('admin')) {
+	throw new Exception('401 Unauthorized');
+}
+?>
+<div class="col-xs-12 eqLogic">
+	<ul class="nav nav-tabs">
+		<li role="presentation" class="active">
+			<a href="#eventsTab" role="tab" data-toggle="tab">
+				<i class="fas fa-list-alt"/>
+					Evenements
+			</a>
+		</li>
+		<li role="presentation">
+			<a href="#elementsTab" role="tab" data-toggle="tab">
+				<i class="fas fa-cubes"/>
+					Elements
+			</a>
+
+		</li>
+	</ul>
+	<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+		<div role="tabpanel" class="tab-pane active" id="eventsTab">
+		Evenements
+		</div>
+		<div role="tabpanel" class="tab-pane" id="elementsTab">
+		Elements
+		</div>
+	</div>
+
+</div>
